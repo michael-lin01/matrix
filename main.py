2dd7d579fc7b85f9069a0e2056e05bc952bfa171
Michael Lin <mlin01@stuy.edu>
2020-02-14 11:07:48 -0500

    drawing stuff

--- main.py
+++ main.py
@@ -4,11 +4,20 @@ from matrix import *
 
 screen = new_screen()
 color = [ 0, 0, 255 ]
-matrix = new_matrix()
-add_edge(matrix,200,300,0,300,300,0)
-add_edge(matrix,300,300,0,300,200,0)
-add_edge(matrix,300,200,0,200,200,0)
-add_edge(matrix,200,200,0,200,300,0)
-
+matrix = new_matrix(0,0)
+add_edge(matrix,100,400,0,200,400,0)
+add_edge(matrix,200,400,0,200,300,0)
+add_edge(matrix,200,300,0,100,300,0)
+add_edge(matrix,100,300,0,100,400,0)
 draw_lines( matrix, screen, color )
+
+m1 = new_matrix()
+ident(m1)
+m1[3] = [10,10,0,1]
+print_matrix(m1)
+print_matrix(matrix)
+matrix_mult(m1,matrix)
+draw_lines(matrix,screen,color)
+print_matrix(matrix)
+
 display(screen)
