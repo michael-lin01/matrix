439eba5209ba97e02986efed8e5b1a6d5909654e
Michael Lin <mlin01@stuy.edu>
2020-02-14 10:49:10 -0500

    working draw lines

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from draw import *
 from matrix import *
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
+color = [ 0, 0, 255 ]
 matrix = new_matrix()
-
+add_edge(matrix,200,300,0,300,300,0)
+add_edge(matrix,300,300,0,300,200,0)
+add_edge(matrix,300,200,0,200,200,0)
+add_edge(matrix,200,200,0,200,300,0)
 
 draw_lines( matrix, screen, color )
 display(screen)
