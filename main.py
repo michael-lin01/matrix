0e1041ed81c5ef56b07e98aa3f9cdb54c60479a1
Michael Lin <michaellin01@gmail.com>
2020-02-23 15:52:17 -0500

    finished image

--- main.py
+++ main.py
@@ -3,21 +3,24 @@ from draw import *
 from matrix import *
 
 screen = new_screen()
-color = [ 0, 0, 255 ]
+color = [ 0, 0, 0 ]
 matrix = new_matrix(0,0)
-add_edge(matrix,100,400,0,200,400,0)
-add_edge(matrix,200,400,0,200,300,0)
-add_edge(matrix,200,300,0,100,300,0)
-add_edge(matrix,100,300,0,100,400,0)
+add_edge(matrix,250,330,0,292,330,0)
+add_edge(matrix,391,388,0,373,285,0)
+add_edge(matrix,373,285,0,407,201,0)
+add_edge(matrix,407,201,0,250,141,0)
+add_edge(matrix,250,120,0,292,330,0)
+add_edge(matrix,292,330,0,391,388,0)
+add_edge(matrix,292,330,0,373,285,0)
+add_edge(matrix,373,285,0,268,214,0)
+add_edge(matrix,314,244,0,265,149,0)
+add_edge(matrix,314,244,0,407,201,0)
+add_edge(matrix,250,120,0,265,149,0)
+for i in range(250,266):
+    add_edge(matrix,250,120,0,i,int(60/157*(i-250)+141),0)
 draw_lines( matrix, screen, color )
-
-m1 = new_matrix()
-ident(m1)
-m1[3] = [10,10,0,1]
-print_matrix(m1)
-print_matrix(matrix)
-matrix_mult(m1,matrix)
-draw_lines(matrix,screen,color)
-print_matrix(matrix)
+for point in matrix:
+    point[0] = 500-point[0]
+draw_lines( matrix, screen, color )
 
 display(screen)
